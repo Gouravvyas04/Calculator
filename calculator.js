bba4dd7dd12eff96e3362912d2dd66797124ffef
Gouravvyas04 <160164280+Gouravvyas04@users.noreply.github.com>
2024-04-01 15:56:19 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -2,16 +2,23 @@ var btns = document.querySelectorAll(".btn");
 let input = document.querySelector(".inp");
 let enter = document.querySelector(".enter");
 let clear = document.querySelector(".clr");
+var output = false;
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function () {
     //   input.value = "";
     input.value += btn.textContent;
+    output = true;
+    if(output===true){
+      input.value="";
+      output = false;
+    }
   });
 });
 
 enter.addEventListener("click", () => {
   input.value += ` = ${eval(input.value)}`;
+  output = true;
 });
 
 clear.addEventListener("click", () => {
